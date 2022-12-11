724e3d68bb892871242f88733f85b67330aa867c
Oleh Krotovych <olgastdev@gmail.com>
2022-12-11 15:32:24 +0100

    Create basic form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
@@ -14,7 +15,7 @@ import { AppRoutingModule } from './app-routing.module';
     MortgageInputPageComponent,
     MortgageCalculationPageComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/mortgage-input-page/mortgage-input-page.component.ts
+++ src/app/mortgage-input-page/mortgage-input-page.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'si-mortgage-input-page',
   templateUrl: './mortgage-input-page.component.html',
   styleUrls: ['./mortgage-input-page.component.scss'],
 })
-export class MortgageInputPageComponent {}
+export class MortgageInputPageComponent {
+  mortgageForm = this.formBuilder.group({
+    loanType: 'Mortgage',
+    amount: '',
+    productType: 'MORTGAGE',
+    interestRate: '',
+    loanPercentage: '',
+    years: '',
+    fixation: '',
+    repayment: '',
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmit(): void {
+    console.log('Mortgage:', this.mortgageForm.value);
+  }
+}
