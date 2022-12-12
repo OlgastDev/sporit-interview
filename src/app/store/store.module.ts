a72e4358b2df6d735ab5ca05337805947770880d
Oleh Krotovych <olgastdev@gmail.com>
2022-12-12 20:36:10 +0100

    Initialize empty reducers

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+import { calculationReducer } from './calculation.reducer';
+import { mortgageRangesReducer } from './mortgage-ranges.reducer';
+import { userInputReducer } from './user-input.reducer';
 
+const state = {
+  calculation: calculationReducer,
+  userInput: userInputReducer,
+  mortgageRanges: mortgageRangesReducer,
+};
 @NgModule({
-  imports: [StoreModule.forRoot({})],
+  imports: [StoreModule.forRoot(state)],
   exports: [StoreModule],
 })
 export class AppStoreModule {}
