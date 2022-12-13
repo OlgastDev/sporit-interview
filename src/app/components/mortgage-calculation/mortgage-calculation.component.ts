a8ce780112d3b2fd8c686785776363efaa6d64db
Oleh Krotovych <olgastdev@gmail.com>
2022-12-13 03:44:53 +0100

    Display difference between tip and real value

--- src/app/components/mortgage-calculation/mortgage-calculation.component.ts
+++ src/app/components/mortgage-calculation/mortgage-calculation.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { UserTip } from '../../store/user-input.reducer';
 import { MortgageCalculation } from '../../store/calculation.reducer';
 
+const calcDiff = (first: number, second: number) =>
+  Math.round(Math.abs(first - second) * 100) / 100;
 @Component({
   selector: 'si-mortgage-calculation',
   templateUrl: './mortgage-calculation.component.html',
@@ -10,4 +12,21 @@ import { MortgageCalculation } from '../../store/calculation.reducer';
 export class MortgageCalculationComponent {
   @Input() calculation: MortgageCalculation | null = null;
   @Input() userTip: UserTip | null = null;
+
+  interestRateSuccess(): number | null {
+    if (this.calculation && this.userTip) {
+      return calcDiff(
+        this.calculation?.interestRate,
+        this.userTip?.interestRate
+      );
+    }
+    return null;
+  }
+
+  repaymentSuccess(): number | null {
+    if (this.calculation && this.userTip) {
+      return calcDiff(this.calculation?.repayment, this.userTip?.repayment);
+    }
+    return null;
+  }
 }
